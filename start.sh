c4b3a29de28d1d7b905c52f715bbe2b1866eb6af
АЛЬБИНА <albina@MacBook-Pro-ALBINA.local>
2026-06-14 22:15:57 +0600

    Убран API ключ из start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/Desktop/student_system
 source venv/bin/activate
-ANTHROPIC_API_KEY='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' python3 manage.py runserver 8000
+export ANTHROPIC_API_KEY=$(cat ~/Desktop/student_system/.env | grep ANTHROPIC_API_KEY | cut -d '=' -f2)
